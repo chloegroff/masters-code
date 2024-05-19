188b54780b1ce3d568174d5d0d387486c8c7f872
Chloe Groff <chloegroff19@gmail.com>
2024-05-19 19:58:40 +0100

    updated possible loss functions

--- ReportFigures/modellossplots.py
+++ ReportFigures/modellossplots.py
@@ -8,6 +8,14 @@ with open(rf'C:\Users\chloe\DE4\Masters\Models\Model_{model_num}_history.json', 
 
 if history['Loss Function'] == 'dice_mean_iou':
     loss_func = 'Dice / Mean IoU Loss'
+elif history['Loss Function'] == 'dice_loss':
+    loss_func = 'Dice Loss'
+elif history['Loss Function'] == 'mean_iou_loss':
+    loss_func = 'Mean IoU Loss'
+elif history['Loss Function'] == 'dice_p_bce':
+    loss_func = 'Dice / Weighted Binary Cross Entropy Loss'
+elif history['Loss Function'] == 'weighted_bincrossentropy':
+    loss_func = 'Weighted Binary Cross Entropy Loss'
 
 plt.plot(history['History']['loss'])
 plt.plot(history['History']['val_loss'])
@@ -27,7 +35,7 @@ plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 plt.savefig(rf'C:\Users\chloe\DE4\Masters\Figures\Dice_{model_num}.pdf', dpi =300)
 
-# print("Max acc:", round(max(history.history['acc']),4))
-# print("Max val_acc:", round(max(history.history['val_acc']),4))
-# print("\nMin loss:", round(min(history.history['loss']),4))
-# print("Min val_loss:", round(min(history.history['val_loss']),4))
+print("Max dice coef:", round(max(history['History']['dice_coef']),4))
+print("Max dice coef:", round(max(history['History']['val_dice_coef']),4))
+print("\nMin loss:", round(min(history['History']['loss']),4))
+print("Min val_loss:", round(min(history['History']['val_loss']),4))
